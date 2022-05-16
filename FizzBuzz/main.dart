4c739dbe21daa81bc0a7c2ab661175f49263de9a
GitGud31 <mhamedlehbab@gmail.com>
2022-05-16 10:53:37 +0100

    add test: for fizz buzz

--- FizzBuzz/main.dart
+++ FizzBuzz/main.dart
@@ -1,3 +1,28 @@
+/**
+ * Write a program that prints one line for each number from 1 to 100
+  For multiples of three print Fizz instead of the number
+  For the multiples of five print Buzz instead of the number
+  For numbers which are multiples of both three and five print FizzBuzz instead of the number
+ */
+
 void main() {
   print("fizz buzz kata");
+}
+
+class FizzBuzzChecker {
+  int start;
+  int end;
+  String _message;
+
+  FizzBuzzChecker(this.start, this.end) : _message = "";
+
+  String get message => _message;
+
+  void checker() {
+    for (int i = start; i < end; i++) {
+      if (i % 3 == 0) _message = "Fizz";
+      if (i % 5 == 0) _message = "Buzz";
+      if (i % 3 == 0 && i % 5 == 0) _message = "FizzBuzz";
+    }
+  }
 }
--- FizzBuzz/test/main_test.dart
+++ FizzBuzz/test/main_test.dart
@@ -1,1 +1,30 @@
-void main() {}
+import 'package:test/expect.dart';
+import 'package:test/scaffolding.dart';
+
+import '../main.dart';
+
+void main() {
+  test("Check for a Fizz", () {
+    final game = FizzBuzzChecker(1, 4);
+
+    game.checker();
+
+    expect(game.message, "Fizz");
+  });
+
+  test("Check for a Buzz", () {
+    final game = FizzBuzzChecker(1, 6);
+
+    game.checker();
+
+    expect(game.message, "Buzz");
+  });
+
+    test("Check for a FizzBuzz", () {
+    final game = FizzBuzzChecker(1, 16);
+
+    game.checker();
+
+    expect(game.message, "FizzBuzz");
+  });
+}
