8ff9a2f0f980b193aaf7882f8a91387501b32b89
kratos <mhamedlehbab@gmail.com>
2022-05-27 19:11:30 +0100

    Ohce kata refactor

--- ohce_kata/src/Models.java
+++ ohce_kata/src/Models.java
@@ -25,7 +25,10 @@ class OhceBot {
     void running() {
         System.out.println("Type: Stop! to exit.");
 
-        
+        System.out.print("> What's your name: ");
+        this.userName = sc.nextLine();
+        botResponse();
+
         do {
             readInput();
             botResponse();
@@ -34,62 +37,40 @@ class OhceBot {
 
     void readInput() {
         System.out.println();
-        
-        if (this.isFirstMessage) {
-            System.out.print("> What's your name: ");
-            this.userName = sc.nextLine();
-        } else {
-            System.out.print("$ ");
-            this.userInput = sc.nextLine();
-        }
+
+        System.out.print("$ ");
+        this.userInput = sc.nextLine();
     }
 
     private void botResponse() {
         final int hour = this.time.getHour();
 
-        if (this.userInput.equals(EXIT_BOT)) {
-            System.out.println("Adios " + this.userName);
+        if (this.userInput.equals(EXIT_BOT) || this.userName.equals(EXIT_BOT)) {
+            System.out.println("> Adios " + this.userName);
             return;
         }
 
         if (this.isFirstMessage) {
             this.isFirstMessage = false;
 
-            System.out.println();
             if ((hour > 20 && hour < 24) || (hour > 0 && hour <= 6)) {
                 System.out.print("> " + TWENTY_TO_SIX + " " + this.userName);
-            }
-            if (hour > 6 && hour <= 12) {
+            } else if (hour > 6 && hour <= 12) {
                 System.out.print("> " + SIX_TO_TWELVE + " " + this.userName);
-            }
-            if (hour > 12 && hour <= 20) {
+            } else {
                 System.out.print("> " + TWELVE_TO_TWENTY + " " + this.userName);
             }
+
             System.out.println();
         } else {
 
-            System.out.println();
-            if (isPalindrome()) {
-                System.out.print("> " + this.userInput);
+            final String reversed = new StringBuilder(this.userInput).reverse().toString();
+
+            System.out.println("> " + reversed);
+
+            if (!this.userInput.equals(reversed)) {
                 System.out.print("> " + FOUND_PALINDROME);
-            } else {
-                System.out.print("> " + this.userInput);
             }
         }
-    }
-
-    private boolean isPalindrome() {
-        String reverse = new String();
-        int length = this.userInput.length();
-
-        for (int i = length - 1; i >= 0; i--) {
-            reverse += this.userInput.charAt(i);
-        }
-
-        if (!this.userInput.equals(reverse)) {
-            this.userInput = reverse;
-            return false;
-        }
-        return true;
     }
 }
